536b02e06d308ff1fc44dd438b369dcdc4c2eb57
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-08-09 14:12:23 -0300

    🚧 ✨ Criando as avaliações

--- itemMan.tsx
+++ itemMan.tsx
@@ -3,6 +3,7 @@ const itemMan = () => [
     id: 1,
     name: "Item 1",
     price: "R$ 1,00",
+    description: "Description 1",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
@@ -10,6 +11,7 @@ const itemMan = () => [
     id: 2,
     name: "Item 2",
     price: "R$ 2,00",
+    description: "Description 2",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
@@ -17,6 +19,7 @@ const itemMan = () => [
     id: 3,
     name: "Item 3",
     price: "R$ 3,00",
+    description: "Description 3",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
@@ -24,6 +27,7 @@ const itemMan = () => [
     id: 4,
     name: "Item 4",
     price: "R$ 4,00",
+    description: "Description 4",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
@@ -31,6 +35,7 @@ const itemMan = () => [
     id: 5,
     name: "Item 5",
     price: "R$ 5,00",
+    description: "Description 5",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
@@ -38,6 +43,7 @@ const itemMan = () => [
     id: 6,
     name: "Item 6",
     price: "R$ 6,00",
+    description: "Description 6",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
@@ -45,6 +51,7 @@ const itemMan = () => [
     id: 7,
     name: "Item 7",
     price: "R$ 7,00",
+    description: "Description 7",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
@@ -52,6 +59,7 @@ const itemMan = () => [
     id: 8,
     name: "Item 8",
     price: "R$ 8,00",
+    description: "Description 8",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
@@ -59,27 +67,31 @@ const itemMan = () => [
     id: 9,
     name: "Item 9",
     price: "R$ 9,00",
+    description: "Description 9",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
   {
     id: 10,
     name: "Item 10",
-    price: "R$ 1,.00",
+    price: "R$ 1,00",
+    description: "Description 10",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
   {
     id: 11,
     name: "Item 11",
-    price: "R$ 1,.00",
+    price: "R$ 1,00",
+    description: "Description 11",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
   {
     id: 12,
     name: "Item 12",
-    price: "R$ 1,.00",
+    price: "R$ 1,00",
+    description: "Description 12",
     image:
       "https://images.lojanike.com.br/320x320/produto/tenis-air-max-flyknit-racer-nn-DJ6106-300-1-11655156149.jpg",
   },
--- src/pages/Man/index.tsx
+++ src/pages/Man/index.tsx
@@ -3,11 +3,74 @@ import Head from "next/head";
 import React from "react";
 import styles from "./styles.module.css";
 import itemMan from "../../../itemMan";
-import Modal from "../../components/Modal";
+import { BsStar, BsStarFill } from "react-icons/bs";
 
 const Man = () => {
-  const [data] = React.useState(itemMan);
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [data] = React.useState<any>(itemMan);
+  const [isItem, setIsItem] = React.useState<boolean>(false);
+  const [image, setImage] = React.useState<string>();
+  const [description, setDescription] = React.useState<string>();
+
+  if (isItem) {
+    return (
+      <div className={styles.mainContainer}>
+        <Head>
+          <title>Item | Ecomerce</title>
+        </Head>
+
+        <div className={styles.containerItem}>
+          <div className={styles.containerDescription}>
+            <h3>Title</h3>
+            <p>{description}</p>
+          </div>
+
+          <div className={styles.itemImage}>
+            <img src={image} alt="item" />
+          </div>
+
+          <div className={styles.mainContainerItemStyle}>
+            <div className={styles.containerItemStyle}>
+              <div className={styles.itemSize}>
+                <h5>Tamanho</h5>
+                <div className={styles.containerSize}>
+                  <div className={styles.size}>PP</div>
+                  <div className={styles.size}>P</div>
+                  <div className={styles.size}>M</div>
+                  <div className={styles.size}>G</div>
+                  <div className={styles.size}>GG</div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.containerStarStyle}>
+              <div className={styles.itemSize}>
+                <span>Avaliações</span>
+                <div>
+                  <div className={styles.containerStars}>
+                    <div className={styles.star}>
+                      <BsStar size={20} />
+                    </div>
+                    <div className={styles.star}>
+                      <BsStar size={20} />
+                    </div>
+                    <div className={styles.star}>
+                      <BsStar size={20} />
+                    </div>
+                    <div className={styles.star}>
+                      <BsStar size={20} />
+                    </div>
+                    <div className={styles.star}>
+                      <BsStar size={20} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.mainArea}>
@@ -18,7 +81,14 @@ const Man = () => {
         <>
           <div className={styles.container}>
             <div className={styles.item}>
-              <div className={styles.image} onClick={() => setIsOpen(true)}>
+              <div
+                className={styles.image}
+                onClick={() => {
+                  setIsItem(true);
+                  setImage(item.image);
+                  setDescription(item.description);
+                }}
+              >
                 <img
                   key={item.id}
                   width={200}
@@ -33,8 +103,6 @@ const Man = () => {
           </div>
         </>
       ))}
-
-      {isOpen && <Modal setIsOpen={setIsOpen} />}
     </div>
   );
 };
